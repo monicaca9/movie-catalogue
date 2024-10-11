199b10fd563e06b5161ac4777e9c122aa53291b3
monicaoktaviani <116496048+monicaca9@users.noreply.github.com>
2024-10-11 16:00:02 +0700

    movie catalogue

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: '1d34cfcac8b9899530bd2c831fa2638e',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
